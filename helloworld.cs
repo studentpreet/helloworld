ed952bf4533294667e6df6f90195d0a26aadf8da
studentpreet <happylearn95@outlook.com>
2022-10-31 13:12:05 +0530

    Fix some issues
    hello to the world of c sharp

---
+++ helloworld.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Ling;
+using System.Text;
+using System.Threading.Tasks;
+ 
+ namespace HelloWorld
+ {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+ }
